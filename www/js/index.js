3fba6181df8ff0c0bdda9a20c76285dab96a4640
angel.teran <angel.teran@elasthink.com>
2019-04-27 12:50:14 +0200

    Cambios.

--- www/js/index.js
+++ www/js/index.js
@@ -1,25 +1,21 @@
 var app = {
-    init: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    onDeviceReady: function() {
-        if (StatusBar) {
-            StatusBar.overlaysWebView(true);
-            StatusBar.backgroundColorByHexString('#33000000');
-        }
-
+    
+    start: function() {
+        
         var headerEl = document.querySelector('.header'),
             headerBackEl = headerEl.querySelector('.header__back'),
             contentHeadEl = document.querySelector('.content-head'),
-            contentHeadHeight = contentHeadEl.offsetHeight,
             contentBodyEl = document.querySelector('.content-body'),
-            scrollFix = contentHeadEl.offsetHeight - headerEl.offsetHeight;
+            contentHeadHeight, scrollFix;
+        
+        var resize = function() {
+            app.debugSizes();
+            contentHeadHeight = contentHeadEl.offsetHeight;
+            scrollFix = contentHeadHeight - headerEl.offsetHeight;
+            contentBodyEl.style.paddingTop = scrollFix + 'px';
+        };
+        window.addEventListener('resize', resize);
 
-        // Init
-        contentBodyEl.style.paddingTop = scrollFix + 'px';
-
-        // Scroll
         var scroll = function(event) {
             var alpha = contentBodyEl.scrollTop / scrollFix;
             headerBackEl.style.opacity = (alpha < 0 ? 0 : alpha > 1 ? 1 : alpha).toFixed(2);
@@ -32,9 +28,39 @@ var app = {
             }
         };
         contentBodyEl.addEventListener('scroll', scroll);
+        
+        if (StatusBar) {
+            window.requestAnimationFrame(function() {
+                StatusBar.overlaysWebView(true);
+                StatusBar.backgroundColorByHexString('#33000000');
+            }, 50);
+        }
+        
+        resize();
         scroll();
+    },
+    
+    traces: [],
+    
+    debug: function(msg) {
+        this.traces.push('[' + performance.now() + '] ' + msg);
+    },
+    
+    debugSizes: function() {
+        this.debug('windowHeight: ' + window.innerHeight);
+        this.debug('headerHeight: ' + document.querySelector('.header__safe').offsetHeight);
     }
 
 };
 
-app.init();
+(function() {
+    var ready = 0,
+    check = function(event) {
+        app.debug(event.type);
+        if (++ready === 2) {
+            app.start();
+        }
+    };
+    document.addEventListener('deviceready', check);
+    document.addEventListener('DOMContentLoaded', check);
+})();
